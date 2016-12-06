1f8c995b1f7af16d910efdc37246bba9a5a27267
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-12-06 20:32:57 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,25 +2,25 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  def self.table_name
-  self.to_s.downcase.pluralize
-  end
-  def self.column_names
-  DB[:conn].results_as_hash = true
-  sql="pragma table_info('#{table_name}')"
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
-  end
+def self.table_name
+self.to_s.downcase.pluralize
+end
+def self.column_names
+DB[:conn].results_as_hash = true
+sql="pragma table_info('#{table_name}')"
+table_info = DB[:conn].execute(sql)
+column_names = []
+table_info.each do |row|
+column_names << row["name"]
+end
+column_names.compact
+end
 self.column_names.each do |col_name|
-  attr_accessor col_name.to_sym
+attr_accessor col_name.to_sym
 end
 def initialize(options={})
 options.each do |property , value|
-  self.send("#{property}=",value)
+self.send("#{property}=",value)
 end
 end
 def save
@@ -29,28 +29,27 @@ DB[:conn].execute(sql)
 @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 end
 def table_name_for_insert
-  self.class.table_name
+self.class.table_name
 end
 def values_for_insert
-  values = []
-  self.class.column_names.each do |col_name|
-    values << "'#{send(col_name)}'" unless send(col_name).nil?
-  end
-  values.join(", ")
+values = []
+self.class.column_names.each do |col_name|
+values << "'#{send(col_name)}'" unless send(col_name).nil?
+end
+values.join(", ")
 end
 def col_names_for_insert
-  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-end
-
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-  DB[:conn].execute(sql)
+self.class.column_names.delete_if {|col| col == "id"}.join(", ")
 end
 def self.find_by(attr)
-  if !  attr.first[1].is_a?(Integer)
-     row=DB[:conn].execute("select * from #{self.table_name} where #{attr.first.first.to_s} = ? ",attr.first[1].to_s)
-    else
-      row=DB[:conn].execute("select * from #{self.table_name} where #{attr.first.first.to_s}= ? ",attr.first[1].to_i)
-    end
+if !  attr.first[1].is_a?(Integer)
+row=DB[:conn].execute("select * from #{self.table_name} where #{attr.first.first.to_s} = ? ",attr.first[1].to_s)
+else
+row=DB[:conn].execute("select * from #{self.table_name} where #{attr.first.first.to_s}= ? ",attr.first[1].to_i)
+end
+end
+def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
 end
